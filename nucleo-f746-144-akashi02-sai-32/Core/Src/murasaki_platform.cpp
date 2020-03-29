d198c122eecc32ba645ff116f6635a29ec297499
takemasa <takemasa@vm>
2020-03-29 12:48:20 +0900

    Update to the newest murasaki.

--- nucleo-f746-144-akashi02-sai-32/Core/Src/murasaki_platform.cpp
+++ nucleo-f746-144-akashi02-sai-32/Core/Src/murasaki_platform.cpp
@@ -48,7 +48,6 @@ extern SAI_HandleTypeDef hsai_BlockB1;
 /* -------------------- PLATFORM Prototypes ------------------------- */
 
 void TaskBodyFunction(const void *ptr);
-void I2cSearch(murasaki::I2CMasterStrategy *master);
 
 /* -------------------- PLATFORM Implementation ------------------------- */
 
@@ -91,7 +90,7 @@ void InitPlatform()
 
     // Initialize audio peripheral
     // By Akashi02 board design, SAI1_B and SAI1_A are assigned to RX and TX, respectively.
-    murasaki::platform.sai = new murasaki::SaiPortAdaptor(
+    murasaki::platform.sai = new murasaki::SaiPortAdapter(
                                                           &hsai_BlockB1, /* TX */
                                                           &hsai_BlockA1); /* RX */
     MURASAKI_ASSERT(nullptr != murasaki::platform.sai)
@@ -647,42 +646,3 @@ void TaskBodyFunction(const void *ptr) {
     }
 
 }
-
-/**
- * @brief I2C device serach function
- * @param master Pointer to the I2C master controller object.
- * @details
- * Poll all device address and check the response. If no response(NAK),
- * there is no device.
- *
- * This function can be deleted if you don't use.
- */
-#if 1
-void I2cSearch(murasaki::I2CMasterStrategy *master)
-               {
-    uint8_t tx_buf[1];
-
-    murasaki::debugger->Printf("\n            Probing I2C devices \n");
-    murasaki::debugger->Printf("   | 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
-    murasaki::debugger->Printf("---+------------------------------------------------\n");
-
-    // Search raw
-    for (int raw = 0; raw < 128; raw += 16) {
-        // Search column
-        murasaki::debugger->Printf("%2x |", raw);
-        for (int col = 0; col < 16; col++) {
-            murasaki::I2cStatus result;
-            // check whether device exist or not.
-            result = master->Transmit(raw + col, tx_buf, 0);
-            if (result == murasaki::ki2csOK)  // device acknowledged.
-                murasaki::debugger->Printf(" %2X", raw + col);  // print address
-            else if (result == murasaki::ki2csNak)  // no device
-                murasaki::debugger->Printf(" --");
-            else
-                murasaki::debugger->Printf(" ??");  // unpredicted error.
-        }
-        murasaki::debugger->Printf("\n");
-    }
-
-}
-#endif
